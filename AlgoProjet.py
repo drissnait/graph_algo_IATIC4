912b6002212cb76670b39f94786799f840a42803
siditfeil <sidimoctar.tfeil@gmail.com>
2020-12-24 00:54:24 +0100

    Ajout version 24/12

--- AlgoProjet.py
+++ AlgoProjet.py
@@ -11,10 +11,13 @@ import matplotlib.image as mpimg
 from networkx.generators.random_graphs import erdos_renyi_graph
 import os
 from PIL import Image #Afficher les images
+import numpy as np
 
 """
 Pour lancer le programme : python AlgoProjet.py [nbr de sommets] [fichierCSV à traiter]
-exemple : python AlgoProjet.py 10 GrapheGenere.csv
+exemple : python AlgoProjet.py 10 choix GrapheGenere.csv
+choix = 1 EdgarGilbert
+choix = 2 BarabasiAlbert
 """
 
 """
@@ -24,6 +27,23 @@ def generationGrapheAleatoireEdgarGilbert():
     n= int(sys.argv[1])
     p=0.5
     g=erdos_renyi_graph(n,p)
+    EnregistrerGrapheAlea(g)
+
+"""
+Generer un graphe aleatoire avec la methode de edgard Gilbert
+"""
+def generationGrapheAleatoireBarabasiAlbert():
+    n= int(sys.argv[1])
+    # Get parameters
+    #init_nodes = 3
+    final_nodes = n
+    m_parameter = 2
+   
+    g = nx.barabasi_albert_graph(final_nodes, m_parameter)
+    EnregistrerGrapheAlea(g)
+    
+    
+def EnregistrerGrapheAlea(g):
     print("liste des noeuds : ")
     print (g.nodes)
     print("liste des arrêtes : ")
@@ -35,7 +55,7 @@ def generationGrapheAleatoireEdgarGilbert():
         os.remove('GrapheGenere.csv')
     f = open('GrapheGenere.csv','ab')
     listeEntete=["id_1","id_2"]
-    writer=csv.writer(f, delimiter=";" )
+    writer=csv.writer(f, delimiter="," )
     writer.writerow(listeEntete)
     for i in listeArretes:
         sommet1=str(i).split(",")[0]
@@ -46,19 +66,18 @@ def generationGrapheAleatoireEdgarGilbert():
         sommet2=sommet2.strip()
         f = open('GrapheGenere.csv','ab')
         with f as csvfile:
-            writer=csv.writer(csvfile, delimiter=";" )
+            writer=csv.writer(csvfile, delimiter="," )
             listeSommetInsert=[sommet1,sommet2]
             writer.writerow(listeSommetInsert)
     plt.close()
     f.close()
 
-
 """
 Afficher le graphe aleatoire genere dans la methode precedente (stocké dans l'image graph.png)
 """
 def showGeneratedGraph():
-    img = Image.open('graph.png')
-    img.show()
+   img = Image.open('graph.png')
+   img.show()
 
 """
 Etude des fichiers de donnees csv (deuxieme partie du projet)
@@ -67,30 +86,26 @@ Etude des fichiers de donnees csv (deuxieme partie du projet)
 #Les fichiers de lecture sont des fichiers csv qui avec id_1 & id_2 comme entete
 ######################################################################################
 def readFile():
-    dataFile= sys.argv[2]
-    dfData=pd.read_csv(dataFile, sep=";")
+    dataFile= sys.argv[3]
+    dfData=pd.read_csv(dataFile, sep=",")
     listeSommets1=dfData.id_1
     listeSommets1SR=list(set(listeSommets1))
-
     listeSommets2=dfData.id_2
     listeSommets2SR=list(set(listeSommets2))
     listeSommets=listeSommets1SR+listeSommets2SR
     listeSommets=list(set(listeSommets))
+    
     print("Nombre de sommets  : " + str(len(listeSommets)))
-    print("Nombre d'arrêtes : " + str(dfData.shape[0]))
+    print("Nombre d'arrêtes : " + str(dfData.shape[0])) #nombre de lignes
     ##########Degré maximal##########
-    s=listeSommets1[0]
-    max=0
-    maxF=0
-    for i in listeSommets1:
-        if(i==s):
-            max+=1
-            if(max>=maxF):
-                maxF=max
-        else:
-            s=i
-            max=1
-    print("Degré maximal : " + str(maxF))
+    listeSommetsOccurences={}
+    listeS=list(listeSommets1) + list(listeSommets2)
+    for i in listeSommets:
+        listeSommetsOccurences[i] = listeS.count(i)      #nombre d'occurence de chaque sommet
+    sortedDict = sorted(listeSommetsOccurences.values()) #trier la liste par ordre croissant
+    maxF = sortedDict[-1]   #le dernier elemnt de la liste
+    
+    print("Degré maximal : " + str(maxF) )
     degreMoyen= dfData.shape[0]/ float(len(listeSommets))
     print("Degré moyen : " + str(degreMoyen))
 
@@ -99,39 +114,39 @@ def readFile():
 Ditribution de degres sous forme de graphique (Qst 2.5)
 """
 def graphique():
-    dataFile= sys.argv[2]
-    dfData=pd.read_csv(dataFile, sep=";")
+    dataFile= sys.argv[3]
+    dfData=pd.read_csv(dataFile, sep=",")
     listeSommetsOccurences={}
-    listeSommets=dfData.id_1
-    listeSommets2=dfData.id_2
-    valOccurence=0
-    for i in listeSommets:
-        if i in listeSommetsOccurences.keys():
-            valOccurence=valOccurence+1
-            listeSommetsOccurences[i] = valOccurence
-        else:
-            valOccurence=1
-            listeSommetsOccurences[i]=valOccurence
+    listeSommets1=list(dfData.id_1)
+    listeSommets2=list(dfData.id_2)
+    listeSommets=listeSommets1 + listeSommets2
 
-    for i in listeSommets2:
-        if i in listeSommetsOccurences.keys():
-            valOccurence=valOccurence+1
-            listeSommetsOccurences[i] = valOccurence
-        else:
-            valOccurence=1
-            listeSommetsOccurences[i]=valOccurence
+    for i in listeSommets:
+        listeSommetsOccurences[i] = listeSommets.count(i)
+
 
 
     plt.scatter(listeSommetsOccurences.keys(), listeSommetsOccurences.values(), c='r')
     plt.plot(listeSommetsOccurences.keys(), listeSommetsOccurences.values(), label='f(x)')
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel('degre')
+    plt.ylabel('frequence')
     plt.show()
 
 
 
 def main():
-    generationGrapheAleatoireEdgarGilbert()
+    choix = int(sys.argv[2])
+    if(choix==1):
+        print("Modéle Edgar Gilbert")
+        generationGrapheAleatoireEdgarGilbert()
+    elif(choix==2):
+        print("Modéle Barbasi-Albert")
+        generationGrapheAleatoireBarabasiAlbert()
+    else:
+       print("choix invalide")
+       sys.exit()
+    
+    #
     showGeneratedGraph()
     readFile()
     graphique()
